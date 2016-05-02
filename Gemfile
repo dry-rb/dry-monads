f8b30a289fc050d1fcc904969d04db742c202448
Tim Riley <tim@openmonkey.com>
2016-05-02 19:17:46 +1000

    Set up coverage checking at very top of spec_helper
    This might fix the issue where method definitions aren’t reported as covered.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ source 'https://rubygems.org'
 gemspec
 
 group :test do
-  gem 'codeclimate-test-reporter', platform: :rbx
+  gem 'codeclimate-test-reporter', require: false
+  gem 'simplecov', require: false
 end
 
 group :tools do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'dry-monads'
+if RUBY_ENGINE == 'ruby'
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
 
 begin
   require 'pry'
 rescue LoadError
 end
 
-if RUBY_ENGINE == "rbx"
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
+require 'dry-monads'
