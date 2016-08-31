26402572691e0e00e60b6b75c6df69bcd9212f90
Hannes Maack <hmaack@rocknruby.it>
2016-08-31 14:59:10 +0200

    removed NotImplemended Kernel#fork block to be valid for JRuby

--- spec/integration/monads_serialization_spec.rb
+++ spec/integration/monads_serialization_spec.rb
@@ -1,3 +1,8 @@
+require 'json'
+
+# must be required manually to provide the JSON serialization
+require 'json/add/dry/monads/maybe'
+
 RSpec.describe 'JSON serialization' do
   include Dry::Monads::Maybe::Mixin
 
@@ -10,19 +15,7 @@ RSpec.describe 'JSON serialization' do
 
   subject { JSON.load(JSON.dump(example_structure)) }
 
-  it 'cannot deserialize without require the monad class extension' do
-    is_expected.not_to eql(example_structure)
-  end
-
   it 'should rebuild the example structure' do
-    # Need to create a subprocess with Kernel#fork
-    # to have always an unloaded json/add/dry/monads/maybe.rb file
-    Kernel.fork do
-      # must be required manually to provide the JSON serialization
-      # class extension ONLY in the subprocess
-      require 'json/add/dry/monads/maybe'
-
-      is_expected.to eql(example_structure)
-    end
+    is_expected.to eql(example_structure)
   end
 end
