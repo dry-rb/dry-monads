565ccb83a71671dd25bc2d9c0cb16852779447da
Nikita Shilnikov <fg@flashgordon.ru>
2017-03-08 22:36:24 +0300

    Add specs for #bind and #fmap

--- spec/unit/list_spec.rb
+++ spec/unit/list_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe(Dry::Monads::List) do
   subject { list[1, 2, 3] }
 
   describe '#inspect' do
-    it 'dumps list to string' do
+    it 'dumps list to a string' do
       expect(subject.inspect).to eql('List[1, 2, 3]')
     end
   end
 
   describe '#to_s' do
-    it 'dumps list to string' do
+    it 'dumps list to a string' do
       expect(subject.to_s).to eql('List[1, 2, 3]')
     end
 
@@ -38,20 +38,40 @@ RSpec.describe(Dry::Monads::List) do
   end
 
   describe '#to_a' do
-    it 'coerces list to array' do
+    it 'coerces to an array' do
       expect(subject.to_a).to eql([1, 2, 3])
     end
   end
 
   describe '#to_ary' do
-    it 'coerces list to array' do
+    it 'coerces to an array' do
       expect(subject.to_ary)
     end
 
-    it 'allows to split list' do
+    it 'allows to split a list' do
       a, *b = subject
       expect(a).to eql(1)
       expect(b).to eql([2, 3])
+    end
+  end
+
+  describe '#bind' do
+    it 'binds a block' do
+      expect(subject.bind { |x| [x * 2] }).to eql(list[2, 4, 6])
+    end
+
+    it 'binds a proc' do
+      expect(subject.bind(-> x { [x * 2] })).to eql(list[2, 4, 6])
+    end
+  end
+
+  describe '#fmap' do
+    it 'maps a block' do
+      expect(subject.fmap { |x| x * 2 }).to eql(list[2, 4, 6])
+    end
+
+    it 'maps a proc' do
+      expect(subject.fmap(-> x { x * 2 })).to eql(list[2, 4, 6])
     end
   end
 end
