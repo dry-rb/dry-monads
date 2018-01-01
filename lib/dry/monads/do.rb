f7bc9b73c535997a15a2b66ae67909ef87ca639d
Nikita Shilnikov <fg@flashgordon.ru>
2018-01-01 17:42:49 +0300

    Add docs

--- lib/dry/monads/do.rb
+++ lib/dry/monads/do.rb
@@ -11,17 +11,75 @@ module Dry
         end
       end
 
-      def self.for(method)
+      # Generates a module that passes a block to methods
+      # that either unwraps a single-valued monadic value or halts
+      # the execution.
+      #
+      # @example A complete example
+      #
+      #   class CreateUser
+      #     include Dry::Monads::Result::Mixin
+      #     include Dry::Monads::Try::Mixin
+      #     include Dry::Monads::Do.for(:call)
+      #
+      #     attr_reader :user_repo
+      #
+      #     def initialize(:user_repo)
+      #       @user_repo = user_repo
+      #     end
+      #
+      #     def call(params)
+      #       json = yield parse_json(params)
+      #       hash = yield validate(json)
+      #
+      #       user_repo.transaction do
+      #         user = yield create_user(hash[:user])
+      #         yield create_profile(user, hash[:profile])
+      #       end
+      #
+      #       Success(user)
+      #     end
+      #
+      #     private
+      #
+      #     def parse_json(params)
+      #       Try(JSON::ParserError) {
+      #         JSON.parse(params)
+      #       }.to_result
+      #     end
+      #
+      #     def validate(json)
+      #       UserSchema.(json).to_monad
+      #     end
+      #
+      #     def create_user(user_data)
+      #       Try(Sequel::Error) {
+      #         user_repo.create(user_data)
+      #       }.to_result
+      #     end
+      #
+      #     def create_profile(user, profile_data)
+      #       Try(Sequel::Error) {
+      #         user_repo.create_profile(user, profile_data)
+      #       }.to_result
+      #     end
+      #   end
+      #
+      # @param [Array<Symbol>] methods
+      # @return [Module]
+      def self.for(*methods)
         mod = Module.new do
-          define_method(method) do |*args|
-            begin
-              super(*args) do |*ms|
-                unwrapped = ms.map { |m| m.or { halt(m) }.value! }
-                ms.size == 1 ? unwrapped[0] : unwrapped
+          methods.each do |method|
+            class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+              def #{ method }(*)
+                super do |*ms|
+                  unwrapped = ms.map { |m| m.or { halt(m) }.value! }
+                  ms.size == 1 ? unwrapped[0] : unwrapped
+                end
+              rescue Halt => e
+                e.result
               end
-            rescue Halt => e
-              e.result
-            end
+            RUBY
           end
         end
 
