1e000ed09e89d612fdce5d8ae72f68629014173a
Tim Riley <tim@openmonkey.com>
2018-03-27 09:23:34 +1100

    Move definition of Dry::Monads::Result constructors into result.rb

--- lib/dry/monads.rb
+++ lib/dry/monads.rb
@@ -17,7 +17,6 @@ module Dry
     # List of monad constructors
     CONSTRUCTORS = [
       Maybe::Mixin::Constructors,
-      Result::Mixin::Constructors,
       Validated::Mixin::Constructors,
       Try::Mixin::Constructors,
       Task::Mixin::Constructors,
@@ -28,10 +27,6 @@ module Dry
     Some = Maybe::Some
     # @see Maybe::None
     None = Maybe::None
-    # @see Result::Success
-    Success = Result::Success
-    # @see Result::Failure
-    Failure = Result::Failure
     # @see Validated::Valid
     Valid = Validated::Valid
     # @see Validated::Invalid
@@ -44,41 +39,6 @@ module Dry
       super
 
       base.include(*CONSTRUCTORS)
-    end
-
-    # Creates a module that has two methods: `Success` and `Failure`.
-    # `Success` is identical to {Result::Mixin::Constructors#Success} and Failure
-    # rejects values that don't conform the value of the `error`
-    # parameter. This is essentially a Result type with the `Failure` part
-    # fixed.
-    #
-    # @example using dry-types
-    #   module Types
-    #     include Dry::Types.module
-    #   end
-    #
-    #   class Operation
-    #     # :user_not_found and :account_not_found are the only
-    #     # values allowed as failure results
-    #     Error =
-    #       Types.Value(:user_not_found) |
-    #       Types.Value(:account_not_found)
-    #
-    #     def find_account(id)
-    #       account = acount_repo.find(id)
-    #
-    #       account ? Success(account) : Failure(:account_not_found)
-    #     end
-    #
-    #     def find_user(id)
-    #       # ...
-    #     end
-    #   end
-    #
-    # @param error [#===] the type of allowed failures
-    # @return [Module]
-    def self.Result(error, **options)
-      Result::Fixed[error, **options]
     end
   end
 end
--- lib/dry/monads/result.rb
+++ lib/dry/monads/result.rb
@@ -1,5 +1,4 @@
 require 'dry/equalizer'
-require 'dry/core/constants'
 
 require 'dry/monads/undefined'
 require 'dry/monads/right_biased'
@@ -280,6 +279,48 @@ module Dry
 
         include Constructors
       end
+    end
+
+    extend Result::Mixin::Constructors
+
+    # @see Result::Success
+    Success = Result::Success
+    # @see Result::Failure
+    Failure = Result::Failure
+
+        # Creates a module that has two methods: `Success` and `Failure`.
+    # `Success` is identical to {Result::Mixin::Constructors#Success} and Failure
+    # rejects values that don't conform the value of the `error`
+    # parameter. This is essentially a Result type with the `Failure` part
+    # fixed.
+    #
+    # @example using dry-types
+    #   module Types
+    #     include Dry::Types.module
+    #   end
+    #
+    #   class Operation
+    #     # :user_not_found and :account_not_found are the only
+    #     # values allowed as failure results
+    #     Error =
+    #       Types.Value(:user_not_found) |
+    #       Types.Value(:account_not_found)
+    #
+    #     def find_account(id)
+    #       account = acount_repo.find(id)
+    #
+    #       account ? Success(account) : Failure(:account_not_found)
+    #     end
+    #
+    #     def find_user(id)
+    #       # ...
+    #     end
+    #   end
+    #
+    # @param error [#===] the type of allowed failures
+    # @return [Module]
+    def self.Result(error, **options)
+      Result::Fixed[error, **options]
     end
 
     class Task
