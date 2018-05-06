5b48e83683fe4c3f8090808754858b2d9d3ddea4
Tim Riley <tim@openmonkey.com>
2018-05-07 09:19:27 +1000

    Fix up the conversion stubs specs
    They no longer cause other parts of the suite to fail

--- spec/integration/conversion_stubs_spec.rb
+++ spec/integration/conversion_stubs_spec.rb
@@ -1,72 +1,144 @@
 RSpec.describe "Conversion method stubs raising errors" do
-  before do
-    @loaded_features = $LOADED_FEATURES.dup
-    $LOADED_FEATURES.delete_if { |path| path.include?("dry/monads") }
-
-    Dry::Monads.constants.each do |const|
-      Dry::Monads.send(:remove_const, const)
-    end
-    Dry.send(:remove_const, :Monads)
-  end
-
-  after do
-    require "dry/monads/all"
-  end
-
   describe "Result" do
-    before do
-      require "dry/monads/result"
+    after do
+      re_require "maybe", "validated"
     end
 
     describe "Success" do
-      specify { expect { Dry::Monads::Success('foo').to_maybe }.to raise_error(RuntimeError) }
-      specify { expect { Dry::Monads::Success('foo').to_validated }.to raise_error(RuntimeError) }
+      before do
+        # To simulate files like maybe, validated, etc. not being loaded, we
+        # simply remove the methods they add, and then re-require the files
+        # again afterwards to get the methods back in place (see the `after`
+        # hooks)
+        Dry::Monads::Result::Success.remove_method :to_maybe
+        Dry::Monads::Result::Success.remove_method :to_validated
+      end
+
+      specify "#to_maybe" do
+        expect { Dry::Monads::Success('foo').to_maybe }.to raise_error(RuntimeError)
+      end
+
+      specify "#to_validated" do
+        expect { Dry::Monads::Success('foo').to_validated }.to raise_error(RuntimeError)
+      end
     end
 
     describe "Failure" do
-      specify { expect { Dry::Monads::Failure('foo').to_maybe }.to raise_error(RuntimeError) }
-      specify { expect { Dry::Monads::Failure('foo').to_validated }.to raise_error(RuntimeError) }
+      before do
+        Dry::Monads::Result::Failure.remove_method :to_maybe
+        Dry::Monads::Result::Failure.remove_method :to_validated
+      end
+
+      specify "#to_maybe" do
+        expect { Dry::Monads::Failure('foo').to_maybe }.to raise_error(RuntimeError)
+      end
+
+      specify "#to_validated" do
+        expect { Dry::Monads::Failure('foo').to_validated }.to raise_error(RuntimeError)
+      end
     end
   end
 
   describe "Task" do
     before do
-      require "dry/monads/task"
+      Dry::Monads::Task.remove_method :to_maybe
+      Dry::Monads::Task.remove_method :to_result
     end
 
-    specify { expect { Dry::Monads::Task.new { 'foo' }.to_maybe }.to raise_error(RuntimeError) }
-    specify { expect { Dry::Monads::Task.new { 'foo' }.to_result }.to raise_error(RuntimeError) }
+    after do
+      re_require "maybe", "result"
+    end
+
+    specify "#to_maybe" do
+      expect { Dry::Monads::Task.new { 'foo' }.to_maybe }.to raise_error(RuntimeError)
+    end
+
+    specify "#to_result" do
+      expect { Dry::Monads::Task.new { 'foo' }.to_result }.to raise_error(RuntimeError)
+    end
   end
 
   describe "Try" do
-    before do
-      require "dry/monads/try"
+    after do
+      re_require "maybe", "result"
     end
 
-    describe "Success" do
-      specify { expect { Dry::Monads::Try(ZeroDivisionError) { 1/1 }.to_maybe }.to raise_error(RuntimeError) }
-      specify { expect { Dry::Monads::Try(ZeroDivisionError) { 1/1 }.to_result }.to raise_error(RuntimeError) }
+    describe "Value" do
+      before do
+        Dry::Monads::Try::Value.remove_method(:to_maybe)
+        Dry::Monads::Try::Value.remove_method(:to_result)
+      end
+
+      specify "#to_maybe" do
+        expect { Dry::Monads::Try(ZeroDivisionError) { 1/1 }.to_maybe }.to raise_error(RuntimeError)
+      end
+
+      specify "#to_result" do
+        expect { Dry::Monads::Try(ZeroDivisionError) { 1/1 }.to_result }.to raise_error(RuntimeError)
+      end
     end
 
-    describe "Failure" do
-      specify { expect { Dry::Monads::Try(ZeroDivisionError) { 1/0 }.to_maybe }.to raise_error(RuntimeError) }
-      specify { expect { Dry::Monads::Try(ZeroDivisionError) { 1/0 }.to_result }.to raise_error(RuntimeError) }
+    describe "Error" do
+      before do
+        Dry::Monads::Try::Error.remove_method(:to_maybe)
+        Dry::Monads::Try::Error.remove_method(:to_result)
+      end
+
+      specify "#to_maybe" do
+        expect { Dry::Monads::Try(ZeroDivisionError) { 1/0 }.to_maybe }.to raise_error(RuntimeError)
+      end
+
+      specify "#to_result" do
+        expect { Dry::Monads::Try(ZeroDivisionError) { 1/0 }.to_result }.to raise_error(RuntimeError)
+      end
     end
   end
 
   describe "Validated" do
-    before do
-      require "dry/monads/validated"
+    after do
+      re_require "maybe", "result"
     end
 
     describe "Valid" do
-      specify { expect { Dry::Monads::Valid.new('foo').to_maybe }.to raise_error(RuntimeError) }
-      specify { expect { Dry::Monads::Valid.new('foo').to_result }.to raise_error(RuntimeError) }
+      before do
+        Dry::Monads::Validated::Valid.remove_method(:to_maybe)
+        Dry::Monads::Validated::Valid.remove_method(:to_result)
+      end
+
+      specify "#to_maybe" do
+        expect { Dry::Monads::Valid.new('foo').to_maybe }.to raise_error(RuntimeError)
+      end
+
+      specify "#to_result" do
+        expect { Dry::Monads::Valid.new('foo').to_result }.to raise_error(RuntimeError)
+      end
     end
 
     describe "Invalid" do
-      specify { expect { Dry::Monads::Invalid.new('foo').to_maybe }.to raise_error(RuntimeError) }
-      specify { expect { Dry::Monads::Invalid.new('foo').to_result }.to raise_error(RuntimeError) }
+      before do
+        Dry::Monads::Validated::Invalid.remove_method(:to_maybe)
+        Dry::Monads::Validated::Invalid.remove_method(:to_result)
+      end
+
+      specify "#to_maybe" do
+        expect { Dry::Monads::Invalid.new('foo').to_maybe }.to raise_error(RuntimeError)
+      end
+
+      specify "#to_result" do
+        expect { Dry::Monads::Invalid.new('foo').to_result }.to raise_error(RuntimeError)
+      end
+    end
+  end
+
+  def re_require(*paths)
+    $LOADED_FEATURES.delete_if { |feature|
+      paths.any? { |path| feature.include?("dry/monads/#{path}.rb") }
+    }
+
+    suppress_warnings do
+      paths.each do |path|
+        require "dry/monads/#{path}"
+      end
     end
   end
 end
