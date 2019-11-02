796288873af547a8e8e763501312fab40be34867
Nikita Shilnikov <fg@flashgordon.ru>
2019-11-02 13:58:45 +0300

    Setup Github Actions for CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,16 +4,10 @@ require 'pathname'
 
 SPEC_ROOT = Pathname(__FILE__).dirname
 
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
-
-  if RUBY_VERSION == latest_mri
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
   end
 end
 
