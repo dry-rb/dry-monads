c56cbb0c1d1a1acea5c904a9cbb88b23787b1754
Nikita Shilnikov <fg@flashgordon.ru>
2021-07-19 23:11:34 +0300

    Add caller info to warning

--- lib/dry/monads/maybe.rb
+++ lib/dry/monads/maybe.rb
@@ -94,7 +94,6 @@ module Dry
       class Some < Maybe
         include Dry::Equalizer(:value!)
         include RightBiased::Right
-        extend Core::Deprecations[:"dry-monads"]
 
         # Shortcut for Some([...])
         #
@@ -133,13 +132,15 @@ module Dry
 
           if next_value.nil?
             if self.class.warn_on_implicit_nil_coercion
-              self.class.warn(
+              Core::Deprecations.warn(
                 "Block passed to Some#fmap returned `nil` and was chained to None. "\
                 "This is literally an unlawful behavior and it will not be supported in "\
                 "dry-monads 2. \nPlease, replace `.fmap` with `.maybe` in places where you "\
                 "expect `nil` as block result.\n"\
                 "You can opt out of these warnings with\n"\
-                "Dry::Monads::Maybe.warn_on_implicit_nil_coercion false"
+                "Dry::Monads::Maybe.warn_on_implicit_nil_coercion false",
+                uplevel: 0,
+                tag: :'dry-monads'
               )
             end
             Monads.None()
