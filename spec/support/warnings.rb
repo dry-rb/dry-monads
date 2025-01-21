94479140c6a3a0e3de771dfd1b17bd733705c601
Nikita Shilnikov <fg@flashgordon.ru>
2025-01-21 11:46:34 +0100

    Raise error on warning about unused block argument

--- spec/support/warnings.rb
+++ spec/support/warnings.rb
@@ -7,4 +7,8 @@ require "warning"
 Warning.ignore(%r{rspec/core})
 Warning.ignore(%r{rspec/mocks})
 Warning.ignore(/codacy/)
-Warning[:experimental] = false if Warning.respond_to?(:[])
+Warning[:experimental] = false
+
+if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("3.4")
+  Warning[:strict_unused_block] = true
+end
